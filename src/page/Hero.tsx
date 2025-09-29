787e8d9ca6226a2924822d189f66a567ec519b82
fauzipradipta <dipta0988@gmail.com>
2025-09-29 15:45:37 +0700

    add Hero

---
+++ src/page/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { personalInfo } from "@/data/mock";
+const Hero = () => {
+  return (
+    <section id="home" className="hero">
+        
+    </section>
+    );
+};
+
+export default Hero;
