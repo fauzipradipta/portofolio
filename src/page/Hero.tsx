627243f98aecc2c84839e6f0ec8de8915d04a0f0
Syawadhilah Pradipta <dipta0988@gmail.com>
2025-10-13 14:30:31 +0700

    working on hero

--- src/page/Hero.tsx
+++ src/page/Hero.tsx
@@ -3,7 +3,7 @@ import { personalInfo } from "@/data/mock";
 const Hero = () => {
   return (
     <section id="home" className="hero">
-        
+        <h1> {personalInfo.name}</h1>
     </section>
     );
 };
