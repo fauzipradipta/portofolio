a2f787e5acc17766cc27e190aa28a61b86f325bc
fauzipradipta <dipta0988@gmail.com>
2025-11-09 23:50:27 +0700

    update responsive

--- src/app/page/About.tsx
+++ src/app/page/About.tsx
@@ -8,11 +8,11 @@ const About = () => {
         <div className="grid-container">
           <div className="text-center mb-16">
             <p className=" title-big mb-4">About Me</p>
-            <div className="about-bio w-full px-2 sm:px-4">
-              <p className="text-body text-justify leading- sm:text-base md:text-lg w-full max-w-xs sm:max-w-md md:max-w-2xl mx-auto leading-relaxed break-words whitespace-normal text-justify sm:text-center ">
+            <div className="about-bio sm:text-base md:text-lg w-full max-w-xs sm:max-w-md md:max-w-2xl mx-auto leading-relaxed break-words whitespace-normal text-justify sm:text-center">
+              <p className="text-body text-justify leading-relaxed ">
                 {aboutMeData.bio}              
               </p>
-              <p className="text-body text-justify leading-relaxed sub-bio about-bio sm:text-base md:text-lg w-full max-w-xs sm:max-w-md md:max-w-2xl mx-auto leading-relaxed break-words whitespace-normal text-justify sm:text-center">
+              <p className="text-body text-justify leading-relaxed sub-bio about-bio">
                 {aboutMeData.subBio}
               </p>
             </div>
